dcb806a090858d0f47ce52b77730e83ebf4460dd
王俊杰 <jack@retzero.com>
2020-11-09 14:57:14 +0800

    update

--- src/test/java/com/baidu/disk/DemoApplicationTests.java
+++ src/test/java/com/baidu/disk/DemoApplicationTests.java
@@ -10,13 +10,9 @@ import java.io.IOException;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Autowired
-	private LinkHelper linkHelper;
-
 	@Test
-	void contextLoads() throws IOException {
-		System.out.println(linkHelper.getLink("0abdc1ba8e64bffdac5d3ccf935a66d6", "86214459-250528-140098086493323", "1604634976", "FDtAERVJoK-DCb740ccc5511e5e8fedcff06b081203-BnxgagkhhZDP32ZDuaHTNfb9I5A=", "3350578322"));
+	void contextLoads() {
+
 	}
 
 }
